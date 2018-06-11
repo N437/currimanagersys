b3b29533e4a2c70a92a3362b78925a2528540fd0
LinShengwzp <wzp129168@outlook.com>
2018-06-11 16:42:12 +0800

    change semester

--- src/main/java/com/winter/mapper/plan_study_courseMapper.java
+++ src/main/java/com/winter/mapper/plan_study_courseMapper.java
@@ -3,5 +3,4 @@ package com.winter.mapper;
 import com.winter.model.plan_study_course;
 
 public interface plan_study_courseMapper extends BaseMapper<plan_study_course> {
-
 }
--- src/main/java/com/winter/mapper/resourceMapper.java
+++ src/main/java/com/winter/mapper/resourceMapper.java
@@ -2,16 +2,4 @@ package com.winter.mapper;
 
 import com.winter.model.resource;
 
-public interface resourceMapper {
-    int deleteByPrimaryKey(String resourceid);
-
-    int insert(resource record);
-
-    int insertSelective(resource record);
-
-    resource selectByPrimaryKey(String resourceid);
-
-    int updateByPrimaryKeySelective(resource record);
-
-    int updateByPrimaryKey(resource record);
-}
+public interface resourceMapper extends BaseMapper<resource> {}
