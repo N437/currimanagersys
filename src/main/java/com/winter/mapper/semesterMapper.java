4eae6ce2e39c071b87ec9a4da74a5e3296e64131
LinShengwzp <wzp129168@outlook.com>
2018-06-11 15:24:15 +0800

    change semester

--- src/main/java/com/winter/mapper/semesterMapper.java
+++ src/main/java/com/winter/mapper/semesterMapper.java
@@ -1,24 +1,5 @@
 package com.winter.mapper;
 import com.winter.model.semester;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
 
-public interface semesterMapper {
-
-    @Delete("delete from semesterid where semesterid = #{semesterid}")
-    int deleteByPrimaryKey(String semesterid);
-
-    @Insert("insert into semesterid values(……)")
-    int insert(semester record);
-    
-    @Insert("")
-    int insertSelective(semester record);
-
-
-    semester selectByPrimaryKey(String semesterid);
-
-    int updateByPrimaryKeySelective(semester record);
-
-    int updateByPrimaryKey(semester record);
+public interface semesterMapper extends BaseMapper<semester> {
 }
